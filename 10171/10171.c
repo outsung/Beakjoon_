95e620bc66487bb1623a86fba1e9d78169e50647
outsung <40460655+outsung@users.noreply.github.com>
2020-01-03 22:52:56 +0900

    01.03

---
+++ 10171/10171.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    printf(
+            "\\    /\\ \n"
+            " )  ( ') \n"
+            "(  /  ) \n"
+            " \\(__)| \n"
+            );
+
+    return 0;
+}
