31f14b2c74f7b184613dff4bfd69d263008c9631
outsung <insung9546@gmail.com>
2020-01-08 16:53:17 +0900

    01.08

---
+++ 10998/10998.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    int input_A = 0;
+    int input_B = 0;
+
+    int res = 0;
+
+    scanf("%d %d", &input_A, &input_B);
+
+    res = input_A * input_B;
+
+    printf("%d", res);
+
+    return 0;
+}
