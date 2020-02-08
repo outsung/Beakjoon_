e99ec983df8e480dff757e3f9127cf8bb454df19
outsung <insung9546@gmail.com>
2020-02-08 20:23:55 +0900

    02.08

--- c/1152/1152.c
+++ c/1152/1152.c
@@ -2,6 +2,18 @@
 
 int main(){
 
+    char input_str[1000000] = {};
+
+    int res = 0;
+    for(;EOF != scanf(" %s", input_str);){
+        if(input_str == " "){
+            continue;
+        }
+        res++;
+    }
+
+    printf("%d", res);
+
     return 0;
 }
 
--- c/2908/2908.c
+++ c/2908/2908.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(){
+
+    char input_A[4] = { 0 };
+    char input_B[4] = { 0 };
+
+    int sangsu_A = 0;
+    int sangsu_B = 0;
+
+    scanf(" %s", input_A);
+    scanf(" %s", input_B);
+
+
+    // 4 3 7
+    for(int i = 0; i < 3; i++){
+        sangsu_A += (input_A[i] - 48) * pow(10, i);
+    }
+    for(int i = 0; i < 3; i++){
+        sangsu_B += (input_B[i] - 48) * pow(10, i);
+    }
+
+    sangsu_A > sangsu_B ? printf("%d", sangsu_A) : printf("%d", sangsu_B);
 
     return 0;
 }
